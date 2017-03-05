959d9b2120d3d53dd97c6f954c311bb628084153
Rafal <rafalchojnacki0502@gmail.com>
2017-03-05 16:04:05 +0100

    Krotka - definicja i przykład

---
+++ Tuples-krotki/Krotka.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+#Krotka (ang. tuple) jest niezmienną listą. Zawartość krotki określamy
+#tylko podczas jej tworzenia. Potem nie możemy już jej zmienić.
+#Używamy nawiasów okrągłych zamiast kwadratowych jak w listach!
+
+words = ("jajko","kielbasa","barszcz",)
+
+print(words[0])
+
+words[1] = "ser"    #Zmiana wywoła błąd
+
+
+#Jak słowniki i listy krotki mogą być zagnieżdzone w sobie
