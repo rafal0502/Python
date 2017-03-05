cd984d912327e8d84619262fef1d7bafd4c9c0db
Rafal <rafalchojnacki0502@gmail.com>
2017-03-05 21:54:50 +0100

    Mapy i filtry

---
+++ Programowanie funkcyjne/Filter.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#to samo co map w zasadzie,tylko działa jak filtr
+nums =[11,22,33,44,55]
+res = list(filter(lambda x: x%2==0,nums))
+print(res)
---
+++ Programowanie funkcyjne/Mapy.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#map używamy z agrumentem list (albo innym typu iterable) by przejść po
+#każdym
+
+def add_five(x):
+    return x + 5
+
+nums = [11,22,33,44,55]
+result = list(map(add_five,nums))
+print(result)
+
+
+#z lambdą
+
+result = list(map(lambda x: x+5,nums))
+print(result)
