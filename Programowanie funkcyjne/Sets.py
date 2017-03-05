d0257d309feb6b194ce36892eec2b541d53d79e9
Rafal <rafalchojnacki0502@gmail.com>
2017-03-05 23:08:59 +0100

    Operacje na zbiorach

--- Programowanie funkcyjne/Sets.py
+++ Programowanie funkcyjne/Sets.py
@@ -14,3 +14,23 @@ print(3 in num_set)
 print("spam" not in word_set)
 
 #By stworzyć pustego seta należy użyć set(), bo puste {} tworzą słownik.
+
+#Nie są indeksowane
+#Nie zawierają duplikatów
+
+print("=====================")
+nums = {1,2,3,1,4,5,6}
+print(nums)
+nums.add(-7)
+nums.remove(3)
+print(nums)
+
+print("=====================")
+first = {1,2,3,4,5,6}
+second = {4,5,6,7,8,9}
+
+print(first | second)
+print(first & second)
+print(first - second)
+print(second - first)
+print(first ^ second)
