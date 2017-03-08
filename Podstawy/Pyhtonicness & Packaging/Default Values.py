1d65125e39d997e7d2a536d05e46e3b5473302f2
Rafal <rafalchojnacki0502@gmail.com>
2017-03-08 13:34:58 +0100

    Argumenty domyslne

---
+++ Podstawy/Pyhtonicness & Packaging/Default Values.py
@@ -0,0 +1,11 @@
+#parameters of function can given a default values
+
+
+def function(x,y,food="spam"):
+    print(food)
+
+function(1,2)
+function(3,4,"egg")
+
+#in case the argument is passed in,the defaut value is ignored
+
