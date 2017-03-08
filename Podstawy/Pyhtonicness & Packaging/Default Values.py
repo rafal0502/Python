b93130b2c243a0e486cb1943eb7f57afe23d13f2
Rafal <rafalchojnacki0502@gmail.com>
2017-03-08 13:41:10 +0100

    Argumenty domyslne

--- Podstawy/Pyhtonicness & Packaging/Default Values.py
+++ Podstawy/Pyhtonicness & Packaging/Default Values.py
@@ -9,3 +9,13 @@ function(3,4,"egg")
 
 #in case the argument is passed in,the defaut value is ignored
 
+
+# **kwargs - standing for keyword arguments
+
+def my_func(x,y=7, *args, **kwargs):
+    print(kwargs)
+
+my_func(2,3,4,5,6, a=7,b=8)
+
+# a and b are the names of the arguments  that we passed
+# arguments returned by **kwargs are not included in *args
