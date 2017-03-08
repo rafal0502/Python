ff086efdb333254a555d93d0d861145be4f72704
Rafal <rafalchojnacki0502@gmail.com>
2017-03-08 15:37:50 +0100

    Biblioteki zewnętrzne

---
+++ Podstawy/Pyhtonicness & Packaging/Główne biblioteki zewnętrzne/Omówienie.py
@@ -0,0 +1,28 @@
+"""
+Standardowe biblioteki w pythonie zawieraja obszerne funkcjonalności.
+Jednakże do niektórych zadań wymagane jest użycie zewnętrznych bibliotek.
+Django: najbardziej powszechnie używany web framework napisany w Pythonie
+który wspiera takie strony jak Instagram.
+Kolejne to Flask i CherryPy
+
+
+For scraping data from websites the library BeautifulSoup is very useful
+
+matplotlib - biblioteka sluzaca do tworzeia wykresow
+modul NumPy pozwala używać wielowymiarowych tablic ktore sa
+duzo szybsze w użyciu niż rodzime rozwiazania w Pythonie jak
+zagnieżdżone listy. Zawiera również operacje matemtyczne na nich
+
+Biblioteka SciPy zawiera wiele rozbudowanych funkcjonalnosci do
+NumPy
+
+Dla gier 3D używamy biblioteki Panda3D
+Dla gier 2D używamy pygame
+
+
+
+
+
+
+
+"""
