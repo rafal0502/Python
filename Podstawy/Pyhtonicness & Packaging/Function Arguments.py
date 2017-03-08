e554770d971157aca100b51fc01d62eed2e93acc
Rafal <rafalchojnacki0502@gmail.com>
2017-03-08 13:29:46 +0100

    Argumenty funkcji

---
+++ Podstawy/Pyhtonicness & Packaging/Function Arguments.py
@@ -0,0 +1,7 @@
+def function(named_arg, *args): #name args it's just a convention
+    print(named_arg)
+    print(args)
+
+function(1,2,3,4,5)
+
+#*args accessed inside a function as the tuple args
