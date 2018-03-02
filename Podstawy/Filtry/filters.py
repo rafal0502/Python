557dc35e872d72b629fc1c411e58e95181db55b5
Rafal <rafalchojnacki0502@gmail.com>
2018-03-02 16:10:29 +0100

    dodanie filtow

---
+++ Podstawy/Filtry/filters.py
@@ -0,0 +1,20 @@
+# Filter
+
+mylist = [1,2,3,4,5,6,7,8]
+
+
+def even_bool(num):
+    return num%2 == 0
+
+
+
+evens = filter(even_bool,mylist)
+
+print(list(evens))
+
+
+
+lambda num:num%2 == 0
+
+evens2 = filter(lambda num:num%2 == 0,mylist)
+print(list(evens2))
